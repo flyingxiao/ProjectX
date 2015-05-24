8f55416b824a15826de3f81a8097e00075e4efa2
flyingxiao <singalongxiaoxiao@gmail.com>
2015-05-24 20:04:27 +0800

    proxy avaible test

---
+++ src/cn/xiao/crawl/HttpProxyAvailable.java
@@ -0,0 +1,68 @@
+package cn.xiao.crawl;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class HttpProxyAvailable {
+
+	public static void main(String[] args) {
+		
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		
+        try {
+        	HttpHost target = new HttpHost("www.baidu.com", 443, "https");
+            HttpHost proxy = new HttpHost("117.162.116.4", 8123);
+
+            RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+        	
+            // 创建httpget.
+            HttpGet httpget = new HttpGet("www.baidu.com");
+            httpget.setConfig(config);
+            
+            System.out.println("executing request to " + target + " via " + proxy);
+            
+            // 执行get请求.
+            CloseableHttpResponse response = httpclient.execute(target, httpget);
+            try {
+                // 获取响应实体
+                HttpEntity entity = response.getEntity();
+                System.out.println("--------------------------------------");
+                // 打印响应状态
+                System.out.println(response.getStatusLine());
+                if (entity != null) {
+                    // 打印响应内容长度
+                    System.out.println("Response content length: " + entity.getContentLength());
+                    // 打印响应内容
+                    System.out.println("Response content: " + EntityUtils.toString(entity, "UTF-8"));
+                }
+                System.out.println("------------------------------------");
+            } finally {
+                response.close();
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭连接,释放资源
+            try {
+                httpclient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+	}
+
+}
