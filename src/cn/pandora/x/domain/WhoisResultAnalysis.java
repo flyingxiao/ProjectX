a9804cc327c73f798e9525374ae1a8c2d93095a3
flyingxiao <singalongxiaoxiao@gmail.com>
2015-07-26 22:29:25 +0800

    whois result analysis

--- src/cn/pandora/x/domain/WhoisResultAnalysis.java
+++ src/cn/pandora/x/domain/WhoisResultAnalysis.java
@@ -8,12 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.RandomAccessFile;
-import java.io.Writer;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
@@ -29,20 +24,23 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import cn.cnnic.registry.language.LanguageUtil;
+import cn.cnnic.registry.language.exceptions.PunyException;
+
 public class WhoisResultAnalysis implements Callable<String> {
 
-	private String domainName;
+	private String whoisInfo;
 
 	private static ExecutorService threadPool = null;
 	private static RandomAccessFile rf;
 	private static BufferedWriter bufferedWriter_whois;
-	private static BufferedWriter bufferedWriter_whois_fail;
 	private static String offsetFilePath;
+	private String[] properties = {"Creation Date:", "Registry Expiry Date:", "Sponsoring Registrar:", "Sponsoring Registrar IANA ID:", "Registrant ID:", "Registrant Name:", "Registrant Organization:", "Registrant City:", "Registrant Country:", "Registrant Email:", "Registrant Phone:", "Registrant Fax:"};
 
 	private static final Log log = LogFactory.getLog(WhoisResultAnalysis.class);
 
-	public WhoisResultAnalysis(String domainName) {
-		this.domainName = domainName;
+	public WhoisResultAnalysis(String whoisInfo) {
+		this.whoisInfo = whoisInfo;
 	}
 	
 	public static void main(String[] args) throws InterruptedException,	ExecutionException {
@@ -61,8 +59,8 @@ public class WhoisResultAnalysis implements Callable<String> {
 			}
 		}
 		
-		int corePoolSize = 1;
-		int maximumPoolSize = 1;
+		int corePoolSize = 20;
+		int maximumPoolSize = 20;
 		long keepAliveTime = 3000;
 		
 		BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<Runnable>(
@@ -75,16 +73,14 @@ public class WhoisResultAnalysis implements Callable<String> {
 		try {
 			rf = new RandomAccessFile(basePath + "in", "r");
 			bufferedWriter_whois = new BufferedWriter(new FileWriter(basePath
-					+ "whois_result", true));
-			bufferedWriter_whois_fail = new BufferedWriter(new FileWriter(basePath
-					+ "whois_fail", true));
+					+ "whois_info", true));
 		
-			List<String> domainList = null;
+			List<String> whoisList = null;
 		
 			while (!isFinished()) {
 				log.info("---------------start---------------");
-				domainList = readTargets(10);
-				execute(domainList);
+				whoisList = readTargets(10000);
+				execute(whoisList);
 				log.info("---------------end---------------");
 			}
 		} catch (Exception e) {
@@ -105,29 +101,17 @@ public class WhoisResultAnalysis implements Callable<String> {
 					e.printStackTrace();
 				}
 			}
-			if (bufferedWriter_whois_fail != null) {
-				try {
-					bufferedWriter_whois_fail.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 		
 		log.info("done");
 	}
 	
-	public static void execute(List<String> domainList) {
+	public static void execute(List<String> whoisList) {
 		List<Future<String>> result = new ArrayList<Future<String>>();
-		for (String d : domainList) {
-			DomainQuery task = new DomainQuery(d);
+		for (String w : whoisList) {
+			WhoisResultAnalysis task = new WhoisResultAnalysis(w);
 			Future<String> callable = (Future<String>) threadPool.submit(task);
 			result.add(callable);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
 		}
 
 		String whoisInfo = null;
@@ -157,88 +141,38 @@ public class WhoisResultAnalysis implements Callable<String> {
 					e.printStackTrace();
 				}
 			}
-			if (bufferedWriter_whois_fail != null) {
-				try {
-					bufferedWriter_whois_fail.flush();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
 		}
 	}
 
 	@Override
 	public String call() {
-		String serverName = "whois.afilias.info";
-		String whoisContent = null;
+		String[] whoisContent = whoisInfo.split("\n");
+		String punycode = whoisContent[0].replace("Punycode:", "").trim();
+		String domainName = null;
 		try {
-			int retry = 1;
-			for (; retry <= 5; retry++) {
-				try {
-					whoisContent = queryWhoisServer(domainName, serverName,	43);
-				} catch (SocketTimeoutException e) {
+			domainName = LanguageUtil.punycode2Chinese(punycode);
+		} catch (PunyException e) {
+			e.printStackTrace();
+		}
+		StringBuilder result = new StringBuilder();
+		result.append(domainName).append(",").append(punycode).append(",");
+		
+		for (String property : properties) {
+			String itemValue = null;
+			for (String item : whoisContent) {
+				if (item == null || "".equals(item)) {
 					continue;
 				}
-				break;
+				if (item.startsWith(property)) {
+					itemValue = item.replace(property, "").replace(",", "").trim();
+					break;
+				}
 			}
-			if (whoisContent == null) {
-				System.out.println("Read timed out #" + domainName + "#");
-				bufferedWriter_whois_fail.write(domainName+ "\n");
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage() + " #" + domainName + "#");
-			try {
-				bufferedWriter_whois_fail.write(domainName+ "\n");
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
+			result.append(itemValue == null ? "" : itemValue).append(",");
 		}
-		return whoisContent;
-	}
-
-	private String queryWhoisServer(String domainName, String serverName,
-			int port) throws IOException {
-		StringBuilder result = new StringBuilder();
-		Socket theSocket = null;
-		Writer out = null;
-		InputStreamReader in = null;
-
-		try {
-			InetAddress server = InetAddress.getByName(serverName);
-			// ½¨Á¢socket
-			theSocket = new Socket(server, port);
-			// ÉèÖÃ¶Á³¬Ê±
-			theSocket.setSoTimeout(10000);
-			// ´´½¨Êä³öÁ÷
-			out = new OutputStreamWriter(theSocket.getOutputStream(), "UTF-8");
-			out.write(domainName + "\r\n");
-			out.flush();
-
-			// »º³åÊäÈëÁ÷
-			in = new InputStreamReader(theSocket.getInputStream(), "UTF-8");
-
-			char[] buffer = new char[1024];
-			int chars_read;
-
-			// read until stream closes
-			while ((chars_read = in.read(buffer)) != -1) {
-				result.append(buffer, 0, chars_read);
-			}
-		} finally {
-			try {
-				if (out != null) {
-					out.close();
-				}
-				if (in != null) {
-					in.close();
-				}
-				if (theSocket != null) {
-					theSocket.close();
-				}
-			} catch (IOException e) {
-				log.error("IOException_error#" + domainName + "#", e);
-			}
-		}
+		
+		result.append("\n");
+		
 		return result.toString();
 	}
 
@@ -254,26 +188,41 @@ public class WhoisResultAnalysis implements Callable<String> {
 	public static List<String> readTargets(int number) throws IOException {
 		List<String> targets = new ArrayList<String>();
 		Long position = getOffset(offsetFilePath);
-		log.info("Æ«ÒÆÁ¿£º" + position);
+		log.info("åç§»é‡ï¼š" + position);
 		try {
 			rf.seek(position);
+			String line = rf.readLine();
 			for (int i = 0; i < number; i++) {
-				String line = rf.readLine();
-				StringBuffer target = new StringBuffer();
-				while (line != null) {
-					if (line.startsWith("Domain Name:")) {
-						target.append(line);
-					}
-					line = rf.readLine();
-//					targets.add(target);
+				if (line == null) { //file over
+					break;
 				}
+				int count = 0;
+				StringBuffer target = new StringBuffer();
+				while (line != null) { //æ–‡ä»¶å°¾
+					
+					line = new String(line.getBytes("ISO-8859-1"),"GBK");
+					
+					if (count == 0 && !line.startsWith("Punycode: ")) {
+						position = rf.getFilePointer();
+						line = rf.readLine();
+						continue;
+					}
+					if (line.startsWith("Punycode: ") && count > 0) {//ä¸Šä¸€ä¸ªåŸŸåç»“æŸ
+						break;
+					}
+					target.append(line).append("\n");
+					
+					position = rf.getFilePointer();
+					line = rf.readLine();
+					count++;
+				}
+				targets.add(target.toString());
 			}
 		} catch (IOException e) {
 			throw e;
 		} finally {
 			try {
-				position = rf.getFilePointer();
-				// ½«ÎÄ¼şµÄµ±Ç°Æ«ÒÆÁ¿±£´æµ½±¾µØÎÄ¼ş
+				// å°†æ–‡ä»¶çš„å½“å‰åç§»é‡ä¿å­˜åˆ°æœ¬åœ°æ–‡ä»¶
 				recordOffset(position);
 			} catch (IOException e) {
 				throw e;
@@ -284,7 +233,7 @@ public class WhoisResultAnalysis implements Callable<String> {
 
 	/**
 	 * 
-	 * @return µ±Ç°ÎÄ¼şÆ«ÒÆÁ¿
+	 * @return å½“å‰æ–‡ä»¶åç§»é‡
 	 * @throws IOException
 	 * 
 	 */
@@ -321,10 +270,10 @@ public class WhoisResultAnalysis implements Callable<String> {
 	}
 
 	/**
-	 * ¼ÇÂ¼ÎÄ¼şµÄÆ«ÒÆÁ¿
+	 * è®°å½•æ–‡ä»¶çš„åç§»é‡
 	 * 
 	 * @param position
-	 *            Æ«ÒÆÁ¿ÎÄ¼şÂ·¾¶
+	 *            åç§»é‡æ–‡ä»¶è·¯å¾„
 	 * @throws IOException
 	 * 
 	 */
